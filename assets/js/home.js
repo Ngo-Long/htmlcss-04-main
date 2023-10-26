735c2e98285c3652b2a67dbd0148ae9cd95d5652
Ngo-Long <ngolong.hh@gmail.com>
2023-10-27 00:19:17 +0700

    responsive

---
+++ assets/js/home.js
@@ -0,0 +1,7 @@
+// main
+(() => {
+  const pcNav = document.getElementById("pc__nav");
+  const mobileNav = document.getElementById("mobile__nav");
+
+  mobileNav.innerHTML = pcNav.innerHTML;
+})();
